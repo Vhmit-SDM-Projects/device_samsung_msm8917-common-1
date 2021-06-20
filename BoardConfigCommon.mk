944f82d66cc3d6408b5d0f7ad582e526fe2353d0
Batu33TR <batuhanarslanoglu4@gmail.com>
2021-06-20 18:58:23 +0300

    msm8917-common: Update vendor security patch

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -36,7 +36,7 @@ TARGET_BOOTLOADER_BOARD_NAME := RG13A002KU
 TARGET_NO_BOOTLOADER := true
 
 # Security patch level
-VENDOR_SECURITY_PATCH := 2019-04-01
+VENDOR_SECURITY_PATCH := 2020-08-01
 
 # Kernel
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 androidboot.bootdevice=7824900.sdhci lpm_levels.sleep_disabled=1 androidboot.selinux=permissive loop.max_part=7
