bb7695471105f6827a4c0a2393517f1823a65e90
Leonel Artuzi <leonel.artu@gmail.com>
2021-08-18 00:03:07 -0300

    msm8917-common: Enable configs for Advanced Display

--- msm8917.mk
+++ msm8917.mk
@@ -15,7 +15,8 @@
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
     
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
@@ -332,7 +333,8 @@ PRODUCT_PACKAGES += \
     init.bloatware.rc \
     init.wifi.rc \
     fstab.qcom \
-    ueventd.qcom.rc
+    ueventd.qcom.rc \
+    init.msm8917.rc
     
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
