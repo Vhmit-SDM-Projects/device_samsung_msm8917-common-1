8d1e81f37113519148096dcfe9ae9fca36a72cab
Leonel Artuzi <leonel.artu@gmail.com>
2021-08-17 23:48:40 -0300

    msm8917-common: Fixed PowerHAL
    * Power profile title array

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -151,6 +151,11 @@ BOARD_ROOT_EXTRA_SYMLINKS := \
     /vendor/firmware_mnt:/firmware \
     /mnt/vendor/persist:/persist \
 
+# Power
+TARGET_USES_INTERACTION_BOOST := true
+TARGET_POWERHAL_MODE_EXT := $(COMMON_PATH)/power/power-mode.cpp
+BOARD_POWER_CUSTOM_BOARD_LIB := libpower_8937
+
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
 
